5dcea37d78029393569a5b00663d196e821a7e66
Dr1801 <94034720+Dr1801@users.noreply.github.com>
2022-05-12 22:23:12 +0700

    Update portscannerv3.py

--- portscannerv3.py
+++ portscannerv3.py
@@ -1,57 +1,56 @@
-# Version 3 have been remake from version 2, it some improve in SPEED !!!  
-import argparse
+ import argparse
 import socket
 import time
 import sys
 from threading import Thread, Lock
 from queue import Queue
 
-threads = 1400                                                          # Number of simultaneous threads to use, feel free to tweak
-queue = Queue()                                                         # The queue for the threads
-open_ports =  []                                                        # For storing all the open ports
-print_lock = Lock()                                                     # Print lock so only one thread at a time can print
-socket.setdefaulttimeout(0.15)                                          # The timeout when attempting a connection
+threads = 1400                                                         
+queue = Queue()                                                         
+open_ports =  []                                                       
+print_lock = Lock()                                                    
+socket.setdefaulttimeout(0.15)                                    
 
 def scan_ports(port):
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           # Create socket
-        s.connect((host, port))                                         # Attempt connection on specified host and port
-        with print_lock:                                                # Use lock to prevent multiple threads writing at the same time
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+        s.connect((host, port))                                        
+        with print_lock:                                                
             print('Port:',port, 'is open')
-            open_ports.append(port)                                     # Save all open ports for printing later 
+            open_ports.append(port)                                    
         s.close()
-    except (socket.timeout, ConnectionRefusedError):                    # If we get ConnectionRefused exception, do nothing
+    except (socket.timeout, ConnectionRefusedError):                   
         pass
 
 def scan_thread():
     global queue
     while True:
-        worker = queue.get()                                            # Retrieve port number from queue
-        scan_ports(worker)                                              # Scan the port using scan_ports() function
+        worker = queue.get()                                           
+        scan_ports(worker)                                             
         queue.task_done()
 
 def main(host, ports):
     global queue
-    startTime = time.time()                                             # Timestamp to measure the time taken to scan
+    startTime = time.time()                                            
     for t in range(threads):
         t = Thread(target=scan_thread)
         t.daemon = True
-        t.start()                                                       # Start each thread
+        t.start()                                                       
 
-    for worker in ports:                                                # Put each port into the queue
+    for worker in ports:                                                
         queue.put(worker)
 
-    queue.join()                                                        # Wait until are threads are finished
-    runtime = float("%0.2f" % (time.time() - startTime))                # Second timestamp, calculate time taken
+    queue.join()                                                       
+    runtime = float("%0.2f" % (time.time() - startTime))               
     print("Run time: ", runtime, "seconds")
-    lenth = len(open_ports)                                             # Sort the gates from low to high
+    lenth = len(open_ports)                                            
     for i in range(0, lenth - 1):
         for j in range(i + 1, lenth):
             if (open_ports[i] > open_ports[j]):            
                 tmp = open_ports[i]
                 open_ports[i] = open_ports[j]
                 open_ports[j] = tmp
-    print("Summary of open ports at the host", host, open_ports)        # Print a summary of all open ports
+    print("Summary of open ports at the host", host, open_ports)       
 
 if __name__ == "__main__":
     try:
@@ -68,4 +67,4 @@ if __name__ == "__main__":
 
         main(host, ports)
     except KeyboardInterrupt:
-        print("Interrupt received, scanning will stop...")              # Catch keyboard interrupt exception (Ctrl - c)
+        print("Interrupt received, scanning will stop...")              # Ctrl - c
